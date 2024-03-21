32f658955afdfa06ca8c270003055ac52fc4dd61
snrnapa <shino.satoru@gmail.com>
2024-03-21 22:37:01 +0900

    init tailwindcss and prittier

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    //この中の2行を追加
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
