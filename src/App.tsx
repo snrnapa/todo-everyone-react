b4ef287b38b994dfac35f7a78278fcfc49fbc63d
snrnapa <shino.satoru@gmail.com>
2024-03-23 23:37:38 +0900

    ログイン情報を監視して、ホーム画面を出し分けるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import './App.css';
-import { Button, Divider } from '@mui/material';
+import { Button, Card, Divider } from '@mui/material';
 // import SignIn from './components/SignIn';
 // import Register from './components/Register';
 import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './libs/firebase';
+import { User } from 'firebase/auth';
 import SignIn from './components/SignIn';
 import Register from './components/Register';
 // import Home from './components/Home';
 
 function App() {
   const [registerFlg, setRegisterFlg] = useState(false);
-  const [loginUser, setLoginUser] = useState();
+  const [loginUser, setLoginUser] = useState<User | null>(null);
   const dispRegister = () => {
     setRegisterFlg(!registerFlg);
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoginUser(user);
+    onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        setLoginUser(authUser);
+        console.log(authUser.email);
+        console.log(authUser.uid);
       } else {
         console.log('現在はログインしていません');
       }
@@ -30,13 +33,24 @@ function App() {
   return (
     <div className="bg-gray-200">
       <Header />
-      <div className="h-10"></div>
+      <div className="h-20"></div>
 
-      <div className="flex flex-col ">
-        <SignIn />
-        <Button onClick={dispRegister}>初めての登録はこちら</Button>
-        <Divider />
-        {registerFlg ? <Register /> : <div></div>}
+      <div className="flex flex-col p-2">
+        {loginUser != null ? (
+          <Card className="p-3">
+            <p>下記のユーザーでログインしています</p>
+            <p>{loginUser.email}</p>
+
+            <p>{loginUser.uid}</p>
+          </Card>
+        ) : (
+          <div>
+            <SignIn />
+            <Button onClick={dispRegister}>初めての登録はこちら</Button>
+            <Divider />
+            {registerFlg ? <Register /> : <div></div>}
+          </div>
+        )}
       </div>
     </div>
   );
