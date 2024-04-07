737b51c54a14ddd4f2fc3abc70017c202c9cb775
snrnapa <shino.satoru@gmail.com>
2024-04-07 22:37:03 +0900

    react-routerの設定が完了した

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import './App.css';
 import Header from './components/Header';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthenticatedContent from './AuthenticatedContent';
 
 const App = () => {
   return (
-      <div className="h-screen">
+    <div className="h-screen">
+      <BrowserRouter>
         <Header />
         <div className="h-20"></div>
-        <div className="flex flex-col">
-            <AuthenticatedContent />
-        </div>
-      </div>
+        <Routes>
+          <Route path={`/`} element={<AuthenticatedContent />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
