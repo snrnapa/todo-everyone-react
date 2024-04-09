8cabea76ec69d13d8095510962b140fba12a65c7
snrnapa <shino.satoru@gmail.com>
2024-04-09 23:25:21 +0900

    問い合わせ機能を作成

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,12 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { NotePencil, Envelope, PaperPlane } from 'phosphor-react';
 import { IconButton } from '@mui/material';
+import useCurrentUser from '../components/hooks/UseCurrentUser';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../libs/firebase';
+import Swal from 'sweetalert2';
 
 type ContactInput = {
-  from: string;
   context: string;
 };
 
@@ -14,26 +17,42 @@ const Contact = () => {
     reset,
     formState: { errors },
   } = useForm<ContactInput>();
+  const currentUser = useCurrentUser();
+
+  const onSubmit: SubmitHandler<ContactInput> = async (data) => {
+    console.log('お問い合わせ内容を送信しようとしています');
+    console.log(data);
+    console.log(currentUser?.uid);
+    console.log(currentUser?.email);
+
+    try {
+      await addDoc(collection(db, 'contact'), {
+        user_id: currentUser?.uid,
+        user_email: currentUser?.email,
+        context: data.context,
+      });
+      await Swal.fire({
+        title: 'ご連絡いただき、ありがとうございます！',
+        text: '問い合わせ内容を送信しました。トップ画面に戻ります',
+        icon: 'info',
+        confirmButtonText: 'OK',
+        timer: 7000,
+      });
+      window.location.href = 'http://localhost:5173/';
+    } catch (error) {
+      await Swal.fire({
+        title: '送信中にエラーが発生しました。',
+        text: '再度実行してください。それでも解決しない場合は、管理者に問い合わせてください。${error}',
+        icon: 'error',
+        confirmButtonText: 'OK',
+        timer: 7000,
+      });
+    }
+  };
   return (
     <div className="flex flex-col justify-center space-y-5">
       <p className="text-2xl">Contact</p>
-      <form className="space-y-10">
-        <div>
-          <div className="flex space-x-5">
-            <p>あなたのアドレス</p>
-            <Envelope size={32} color="#120fd2" weight="thin" />
-          </div>
-          <textarea
-            {...register('from', {
-              required: '内容を入力してください',
-              maxLength: {
-                value: 50,
-                message: '100文字以内で簡潔に書きましょう',
-              },
-            })}
-            className="border border-blue-200 w-full rounded-md py-2 px-3 mt-1 focus:outline-none focus:ring focus:border-blue-300 placeholder-blue-400"
-          ></textarea>
-        </div>
+      <form className="space-y-10" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <div className="flex space-x-5">
             <p>お問い合わせ内容</p>
@@ -45,14 +64,14 @@ const Contact = () => {
               required: '内容を入力してください',
               maxLength: {
                 value: 200,
-                message: '100文字以内で簡潔に書きましょう',
+                message: '200文字以内で簡潔に書きましょう',
               },
             })}
             className="border border-blue-200 w-full h-96 rounded-md py-2 px-3 mt-1 focus:outline-none focus:ring focus:border-blue-300 placeholder-blue-400"
           ></textarea>
         </div>
         <div className="flex justify-center">
-          <IconButton onClick={() => {}}>
+          <IconButton type="submit" onClick={() => {}}>
             <PaperPlane size={52} color="#120fd2" weight="thin" />
           </IconButton>
         </div>
