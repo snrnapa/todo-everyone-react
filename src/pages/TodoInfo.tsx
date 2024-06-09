e6cdc53e6878c9afdd8d91aeba15cd0ba84153d8
snrnapa <shino.satoru@gmail.com>
2024-06-09 12:47:55 +0900

    コメントを暫定的に表示するようにした

--- src/pages/TodoInfo.tsx
+++ src/pages/TodoInfo.tsx
@@ -3,11 +3,42 @@ import { useParams } from 'react-router-dom';
 import { showErrorAlert } from '../model/Utils';
 import { Card } from '@mui/material';
 
+// Commentの型定義
+interface Comment {
+  ID: number;
+  CreatedAt: string;
+  UpdatedAt: string;
+  DeletedAt: string | null;
+  todo_id: number;
+  user_id: string;
+  text: string;
+}
+
+// TodoInfoの型定義
+interface TodoInfo {
+  id: number;
+  user_id: string;
+  title: string;
+  detail: string;
+  deadline: string; // 期限 (
+  completed: boolean;
+  favorite_count: number;
+  booked_count: number;
+  cheered_count: number;
+  comment_count: number;
+  is_favorite_me: boolean;
+  is_booked_me: boolean;
+  is_cheered_me: boolean;
+  comments: Comment[];
+}
+
 const TodoInfo = () => {
   const params = useParams();
   const id = params.id;
   const token = localStorage.getItem('firebaseToken');
-  const [todoInfo, setTodoInfo] = useState();
+  const [todoInfo, setTodoInfo] = useState<TodoInfo>(null);
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getTodoInfo = async () => {
@@ -21,9 +52,10 @@ const TodoInfo = () => {
         if (!response.ok) {
           throw new Error(`HTTP Error! status : ${response.status} `);
         }
-        const responseData = await response.json();
-        console.log(responseData[0]);
-        setTodoInfo(responseData[0]);
+        const responseData: TodoInfo = await response.json();
+        console.log(responseData);
+        setTodoInfo(responseData);
+        setIsLoading(false);
       } catch (error) {
         showErrorAlert('todoの詳細取得に失敗しました', `${error}`);
       }
@@ -41,6 +73,20 @@ const TodoInfo = () => {
         <div>
           <p className="text-sm">{todoInfo.title}</p>
           <p className="text-gray-400 text-sm">{todoInfo.detail}</p>
+          <p className="text-gray-400 text-sm">{todoInfo.deadline}</p>
+          <p className="text-gray-400 text-sm">{todoInfo.booked_count}</p>
+          <p className="text-gray-400 text-sm">{todoInfo.cheered_count}</p>
+          <p className="text-gray-400 text-sm">{todoInfo.comment_count}</p>
+          <p className="text-gray-400 text-sm">{todoInfo.favorite_count}</p>
+          <p className="text-gray-400 text-sm">{todoInfo.favorite_count}</p>
+          <div className="space-y-3 ">
+            {(todoInfo.comments ?? []).map((comment) => (
+              <Card className="p-2">
+                <p className="text-sm">{comment.user_id}</p>
+                <p className="text-sm">{comment.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </Card>
     </div>
