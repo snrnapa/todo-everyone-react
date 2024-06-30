ee0a7de66c0d84ffa02765ff6f3ca178877d4339
snrnapa <shino.satoru@gmail.com>
2024-06-30 20:10:48 +0900

    URL先を環境変数により切り替えるようにした

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    // 他に必要な環境変数があれば、ここに追加します
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
