4c129440184a06da9f8aa096361478d428bfbaee
snrnapa <shino.satoru@gmail.com>
2024-07-27 18:43:28 +0900

    動きに緩さをつけた

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,28 @@ import AuthenticatedContent from './AuthenticatedContent';
 import Contact from './pages/Contact';
 import Privacy from './pages/Privacy';
 import TodoInfo from './pages/TodoInfo';
+import { motion } from 'framer-motion';
 
 const App = () => {
   return (
     <div className="h-screen">
       <BrowserRouter>
-        <Header />
-        <div className="h-20"></div>
-        <Routes>
-          <Route path={`/`} element={<AuthenticatedContent />} />
-          <Route path={`/contact`} element={<Contact />} />
-          <Route path={`/privacy`} element={<Privacy />} />
-          <Route path={`/todo/:id`} element={<TodoInfo />} />
-        </Routes>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.5 }}
+          className="h-screen flex flex-col"
+        >
+          <Header />
+          <div className="h-20"></div>
+          <Routes>
+            <Route path={`/`} element={<AuthenticatedContent />} />
+            <Route path={`/contact`} element={<Contact />} />
+            <Route path={`/privacy`} element={<Privacy />} />
+            <Route path={`/todo/:id`} element={<TodoInfo />} />
+          </Routes>
+        </motion.div>
       </BrowserRouter>
     </div>
   );
