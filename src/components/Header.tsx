4133fd643781c23e1c3e9ab12f5973f955db0c03
snrnapa <shino.satoru@gmail.com>
2024-07-27 18:25:25 +0900

    現在時刻を表示するようにしました

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { Envelope, SignOut, LockKey, HouseLine } from 'phosphor-react';
 import { showSuccessAlert } from '../model/Utils';
 import { auth } from '../libs/firebase';
+import React, { useEffect } from 'react';
 
 const Header = () => {
   const logout = async () => {
@@ -12,12 +13,28 @@ const Header = () => {
     window.location.reload();
   };
 
+  const [currentDateTime, setCurrentDateTime] = React.useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <header className="fixed top-0 w-full h-12 bg-personaBlue shadow-lg flex items-center justify-center px-4">
+    <header className="fixed top-0 w-full h-16 bg-personaBlue shadow-lg flex items-center justify-center px-4">
       <div className="font-Darumadrop text-xl flex items-center space-x-9 text-gray-200">
-        <Link to="/" className='flex items-center'>
-          <div className="mr-2 text-personaTextWhite">きょう、なにしますか??</div>
-          <HouseLine size={32} className="text-gray-300" />
+        <Link to="/" >
+          <div className='flex items-center'>
+            <div className="mr-2 text-personaTextWhite">きょう、なにしますか??</div>
+            <HouseLine size={32} className="text-gray-300" />
+          </div>
+          <div className=" text-black flex ">
+            <div>{currentDateTime.toLocaleDateString()}</div>
+            :
+            <div>{currentDateTime.toLocaleTimeString()}</div>
+          </div>
         </Link>
         <div className="flex items-center space-x-4">
           <div className="flex flex-col items-center">
